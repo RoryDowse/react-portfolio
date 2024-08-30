3d8248130f10a4c126af4ed45b0967f969bc0b14
Rory Dowse <rorydowse@hotmail.com>
2024-08-30 09:22:31 -0700

    add Header folder and associated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Footer from './components/Footer';
 
 function App() {
