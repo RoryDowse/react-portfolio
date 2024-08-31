433353511dcdc28d49529d422251ed97b4bb2836
Rory Dowse <rorydowse@hotmail.com>
2024-08-30 21:13:11 -0700

    update Project.jsx and add Project.css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,22 @@
-export default function Project() {
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import './Project.css';
+
+export default function Project({ image, title, deployedLink, githubLink }) {
     return (
-        <div>Project content</div>
+        <div className="project-container">
+            <div className="project-image">
+                <img src={image} alt={title} />
+                <div className="overlay">
+                    <a href={deployedLink} className="project-title">
+                        {title}
+                    </a>
+                    <a href={githubLink} className="github-link" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 }
