c8d7f32222ce2adb1b8dfc6c528cdcbdacd9db1f
Rory Dowse <rorydowse@hotmail.com>
2024-09-02 21:38:25 -0700

    update download container color

--- src/components/Header/Name.jsx
+++ src/components/Header/Name.jsx
@@ -6,7 +6,7 @@ import './Name.css';
 export default function Name() {
     return (
         <div className="container-fluid name-background text-white p-3">
-            <h1 className="mb-4">Rory Dowse</h1>
+            <h1 className="mb-4 fs-custom-h1">Rory Dowse</h1>
             <p className="subtitle mb-0">Software Engineer</p>
         </div>
     );
