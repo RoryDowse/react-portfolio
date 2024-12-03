3f3e15c99e2394914e203d93411b29898e6d02be
Rory Dowse <rorydowse@hotmail.com>
2024-12-02 20:58:27 -0700

    add Blog page

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,8 @@
+export default function Blog() {
+    return (
+        <div className="blog-page">
+            <h1>Blog</h1>
+            <p>Read posts about my journey, coding tips, and tech insights.</p>
+        </div>
+    );
+}
