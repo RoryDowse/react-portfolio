09b42ea9984a9d975209d538307725bbc3d4f837
Rory Dowse <rorydowse@hotmail.com>
2024-12-03 18:58:47 -0700

    remove blur and proritize above fold eager loading

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,16 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Project.css';
 
-export default function Project({ image, title, deployedLink, githubLink }) {
+export default function Project({ id, image, title, deployedLink, githubLink }) {
     return (
         <div className="project-container">
             <div className="project-image">
                 <LazyLoadImage 
                     src={image} 
                     alt={title}
-                    effect="blur" 
+                    loading={id === 1 || id === 2 || id === 3 ? "eager" : "lazy"}
                 />
                 <div className="overlay">
                     <a href={deployedLink} className="project-title" target="_blank" rel="noopener noreferrer">
