6d4f3f009e84edfa3f9a5e2825af47ad1f98aa29
Rory Dowse <rorydowse@hotmail.com>
2024-12-03 19:04:45 -0700

    run npm i

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,3 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './Project.css';
@@ -7,10 +6,10 @@ export default function Project({ id, image, title, deployedLink, githubLink }) 
     return (
         <div className="project-container">
             <div className="project-image">
-                <LazyLoadImage 
+                <img 
                     src={image} 
-                    alt={title}
-                    loading={id === 1 || id === 2 || id === 3 ? "eager" : "lazy"}
+                    alt={`Screenshot of ${title}`}
+                    loading={id === 1 ? "eager" : "lazy"}
                 />
                 <div className="overlay">
                     <a href={deployedLink} className="project-title" target="_blank" rel="noopener noreferrer">
