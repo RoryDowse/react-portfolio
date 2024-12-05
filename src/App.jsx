451a0b6182701f7cb118645d90d511310db8e7b9
Rory Dowse <rorydowse@hotmail.com>
2024-12-04 20:12:21 -0700

    add desktop conditional for lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ import Header from './components/Header/Header';
 // Lazy-loaded Footer component
 const Footer = React.lazy(() => import('./components/Footer'));
 
+const isDesktop = window.innerWidth > 1024;
+
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Header /> {/* Displays the developer's name and navigation menu */}
       <Outlet /> {/* Displays the content of the current page */}
-      <Suspense fallback={<div style={{ height: '100px', backgroundColor: '#f8f9fa' }}>Loading footer...</div>}>
+      {!isDesktop ? (
+        <Suspense fallback={<div style={{ height: '100px', backgroundColor: '#f8f9fa' }}>Loading footer...</div>}>
+          <Footer /> {/* Displays links to the developer's profiles */}
+        </Suspense> 
+      ) : (
         <Footer />
-      </Suspense> {/* Displays links to the developer's profiles */}
+      )}
     </>
   );
 }
