02e0d2da03113879dc8e22f881bce7c0cb5d6ffb
Rory Dowse <rorydowse@hotmail.com>
2024-12-04 19:50:57 -0700

    lazy load Proficiencies component

---
+++ src/components/Proficiencies.jsx
@@ -0,0 +1,71 @@
+export default function Proficiencies() {
+    return (   
+        <div className="proficiencies">
+            <h3 className="proficiencies-title">Developer Proficiencies</h3>
+            <div className="proficiency-cards">
+                <div className="proficiency-card">
+                    <h4 className="proficiency-card-title">Front-End Technologies:</h4>
+                    <ul className="proficiency-list">
+                        <li>CSS</li>
+                        <li>AJAX</li>
+                        <li>HTML</li>
+                        <li>jQuery</li>
+                        <li>React.js</li>
+                        <li>Bootstrap</li>
+                        <li>JavaScript</li>
+                        <li>TypeScript</li>
+                    </ul>
+                </div>
+                <div className="proficiency-card">
+                    <h4>Back-End Technologies:</h4>
+                        <ul className="proficiency-list">
+                            <li>SQL</li>
+                            <li>YAML</li>                           
+                            <li>bcrypt</li>
+                            <li>Apollo</li>
+                            <li>Python</li>
+                            <li>NoSQL</li>
+                            <li>Node.js</li>
+                            <li>GraphQL</li>
+                            <li>Express.js</li>
+                            <li>PyMongo</li>
+                            <li>Sequelize</li>
+                            <li>REST APIs</li>
+                            <li>MongoDB</li>
+                            <li>Mongoose</li>
+                            <li>PostgreSQL</li>
+                            <li>JSON Web Token (JWT)</li>
+                            <li>Application Programming Interfaces (API)</li>                         
+                        </ul>
+                    </div>
+                <div className="proficiency-card">
+                    <h4>Development Tools and Methodologies:</h4>
+                    <ul className="proficiency-list">
+                        <li>Git</li>
+                        <li>Vite</li>
+                        <li>OOP</li>
+                        <li>DOM</li>
+                        <li>MERN</li>
+                        <li>Vitest</li>
+                        <li>GitHub</li>
+                        <li>CircleCI</li>
+                        <li>Cypress</li>
+                        <li>Insomnia</li>
+                        <li>Wireframing</li>
+                        <li>Developer Tools</li>
+                        <li>Web Accessibility</li>
+                        <li>Visual Studio Code</li>
+                        <li>MongoDB Compass</li>
+                        <li>Prompt Engineering</li>
+                        <li>Agile Methodologies</li>
+                        <li>Responsive Web Design</li>
+                        <li>Command Line Interface</li>
+                        <li>Test-Driven Development</li>
+                        <li>Continuous Delivery (CD)</li>
+                        <li>Continuous Integration (CI)</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,9 @@
 import { Helmet } from 'react-helmet';
+import React, { Suspense } from 'react';
 import './Resume.css';
+
+// Lazy-loaded Proficiencies component
+const Proficiencies = React.lazy(() => import('../components/Proficiencies'));
 
 export default function Resume() {
     return (
@@ -25,75 +29,10 @@ export default function Resume() {
                 </div>
             </div>
 
-            {/* List of proficiencies */}
-            <div className="proficiencies">
-                <h3 className="proficiencies-title">Developer Proficiencies</h3>
-                <div className="proficiency-cards">
-                    <div className="proficiency-card">
-                        <h4 className="proficiency-card-title">Front-End Technologies:</h4>
-                            <ul className="proficiency-list">
-                                <li>CSS</li>
-                                <li>AJAX</li>
-                                <li>HTML</li>
-                                <li>jQuery</li>
-                                <li>React.js</li>
-                                <li>Bootstrap</li>
-                                <li>JavaScript</li>
-                                <li>TypeScript</li>
-                            </ul>
-                    </div>
-                    <div className="proficiency-card">
-                        <h4>Back-End Technologies:</h4>
-                            <ul className="proficiency-list">
-                                <li>SQL</li>
-                                <li>YAML</li>                           
-                                <li>bcrypt</li>
-                                <li>Apollo</li>
-                                <li>Python</li>
-                                <li>NoSQL</li>
-                                <li>Node.js</li>
-                                <li>GraphQL</li>
-                                <li>Express.js</li>
-                                <li>PyMongo</li>
-                                <li>Sequelize</li>
-                                <li>REST APIs</li>
-                                <li>MongoDB</li>
-                                <li>Mongoose</li>
-                                <li>PostgreSQL</li>
-                                <li>JSON Web Token (JWT)</li>
-                                <li>Application Programming Interfaces (API)</li>                         
-                            </ul>
-                        </div>
-                    <div className="proficiency-card">
-                        <h4>Development Tools and Methodologies:</h4>
-                            <ul className="proficiency-list">
-                                <li>Git</li>
-                                <li>Vite</li>
-                                <li>OOP</li>
-                                <li>DOM</li>
-                                <li>MERN</li>
-                                <li>Vitest</li>
-                                <li>GitHub</li>
-                                <li>CircleCI</li>
-                                <li>Cypress</li>
-                                <li>Insomnia</li>
-                                <li>Wireframing</li>
-                                <li>Developer Tools</li>
-                                <li>Web Accessibility</li>
-                                <li>Visual Studio Code</li>
-                                <li>MongoDB Compass</li>
-                                <li>Prompt Engineering</li>
-                                <li>Agile Methodologies</li>
-                                <li>Responsive Web Design</li>
-                                <li>Command Line Interface</li>
-                                <li>Test-Driven Development</li>
-                                <li>Continuous Delivery (CD)</li>
-                                <li>Continuous Integration (CI)</li>
-
-                            </ul>
-                    </div>
-                </div>
-            </div>
+            {/* Lazy-loaded Proficiencies component */}
+            <Suspense fallback={<div>Loading Proficiencies...</div>}>
+                <Proficiencies />
+            </Suspense>
         </section>
     );
 }
