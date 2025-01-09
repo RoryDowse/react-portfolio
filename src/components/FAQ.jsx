d709bbe6b794a82eb69107f9f4ce7032b1a84662
Rory Dowse <rorydowse@hotmail.com>
2025-01-09 07:17:39 -0700

    update sitemap homepage, add AboutMe, and fix videos date error

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -10,13 +10,9 @@ export default function FAQ() {
                         <button
                             className="accordion-button"
                             type="button"
-                            // Bootstrap will add the "show" class to this button when the accordion is opened
                             data-bs-toggle="collapse"
-                            // Specify the ID of the div that will be shown when the accordion is opened
                             data-bs-target="#collapseOne"
-                            // The value will change when the accordion is used to assist screen readers
                             aria-expanded="true"
-                            // Links the button to the ID of the div that will be shown when the accordion is opened
                             aria-controls="collapseOne"
                         >
                             How can your web development services help my business?
--- src/components/delete.jsx
+++
@@ -1,8 +0,0 @@
- // Bootstrap will add the "show" class to this button when the accordion is opened
- data-bs-toggle="collapse"
- // Specify the ID of the div that will be shown when the accordion is opened
- data-bs-target="#collapseOne"
- // The value will change when the accordion is used to assist screen readers
- aria-expanded="true"
- // Links the button to the ID of the div that will be shown when the accordion is opened
- aria-controls="collapseOne"
