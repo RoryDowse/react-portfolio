3503a1a1f67689322cb013b805fc6592e4810385
Rory Dowse <rorydowse@hotmail.com>
2025-01-11 11:59:53 -0700

    update Employer h2 color

--- src/components/Employer.jsx
+++ src/components/Employer.jsx
@@ -4,11 +4,11 @@ import './Employer.css';
 export default function ProspectiveEmployer() {
   return (
     <section className="prospective-employer container text-center py-5">
-      <h2 className="display-5 text-white">Prospective Employer?</h2>
+      <h2 className="display-5" style={{ color: 'var(--accent-color)'}}>Prospective Employer?</h2>
       <p className="lead mb-4 text-white">
         If you&apos;re looking for a dedicated and experienced developer to join your team, I would love to connect!
       </p>
-      <Link to="/resume" className="btn btn-secondary btn-lg mt-3">View My Resume</Link>
+      <Link to="/resume" className="btn btn-secondary mt-3">View My Resume</Link>
     </section>
   );
 }
