6a0fe87fa2bf8feb5df6e77f3f8662fc8782544a
Rory Dowse <rorydowse@hotmail.com>
2025-01-18 17:50:15 -0700

    add Dictionary DB

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,11 +2,11 @@ import { Helmet } from 'react-helmet';
 import Project from '../components/Project';
 import './Portfolio.css';
 
-import project1Image from '../assets/dev-demand.webp';
-import project2Image from '../assets/finance-flow.webp';
-import project3Image from '../assets/car-loan-payment-calculator.webp';
-import project4Image from '../assets/james-woodrow-violin.webp';
-import project5Image from '../assets/candidate-search.webp';
+import project1Image from '../assets/dictionary-db.webp';
+import project2Image from '../assets/dev-demand.webp';
+import project3Image from '../assets/finance-flow.webp';
+import project4Image from '../assets/car-loan-payment-calculator.webp';
+import project5Image from '../assets/james-woodrow-violin.webp';
 import project6Image from '../assets/weather-dashboard.webp';
 import project7Image from '../assets/keyboard.webp';
 import project8Image from '../assets/metronome.webp';
@@ -17,37 +17,37 @@ const projects = [
     {
         id: 1,
         image: project1Image,
+        title: 'Dictionary Database',
+        deployedLink: 'https://dictionary-db.onrender.com',
+        githubLink: 'https://github.com/RoryDowse/Dictionary-DB',
+    },
+    {
+        id: 2,
+        image: project2Image,
         title: 'DevDemand',
         deployedLink: 'https://devdemand.onrender.com/',
         githubLink: 'https://github.com/RoryDowse/DevDemand',
     },
     {
-        id: 2,
-        image: project2Image,
+        id: 3,
+        image: project3Image,
         title: 'FinanceFlow',
         deployedLink: 'https://finance-flow-f2zu.onrender.com/',
         githubLink: 'https://github.com/RoryDowse/finance-flow',
     },
     {
-        id: 3,
-        image: project3Image,
+        id: 4,
+        image: project4Image,
         title: 'Car Loan Payment Calculator',
         deployedLink: 'https://rorydowse.github.io/car-loan-payment/',
         githubLink: 'https://github.com/RoryDowse/car-loan-payment',
     },
     {
-        id: 4,
-        image: project4Image,
+        id: 5,
+        image: project5Image,
         title: 'James Woodrow Violin',
         deployedLink: 'https://jameswoodrowviolin.com/',
         githubLink: 'https://github.com/RoryDowse/james-woodrow-violin',
-    },
-    {
-        id: 5,
-        image: project5Image,
-        title: 'Candidate Search',
-        deployedLink: 'https://candidate-search-q9zr.onrender.com/',
-        githubLink: 'https://github.com/RoryDowse/candidate-search',
     },
     {
         id: 6,
