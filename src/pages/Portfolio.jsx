bd2e307ef5f8f28371205e23a2c41372d46b1b08
Rory Dowse <rorydowse@hotmail.com>
2025-02-02 09:32:04 -0700

    update portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,12 +2,12 @@ import { Helmet } from 'react-helmet';
 import Project from '../components/Project';
 import './Portfolio.css';
 
-import project1Image from '../assets/dictionary-db.webp';
-import project2Image from '../assets/dev-demand.webp';
-import project3Image from '../assets/finance-flow.webp';
-import project4Image from '../assets/car-loan-payment-calculator.webp';
-import project5Image from '../assets/james-woodrow-violin.webp';
-import project6Image from '../assets/weather-dashboard.webp';
+import project1Image from '../assets/financial-landing-page.webp';
+import project2Image from '../assets/dictionary-db.webp';
+import project3Image from '../assets/dev-demand.webp';
+import project4Image from '../assets/finance-flow.webp';
+import project5Image from '../assets/car-loan-payment-calculator.webp';
+import project6Image from '../assets/james-woodrow-violin.webp';
 import project7Image from '../assets/keyboard.webp';
 import project8Image from '../assets/metronome.webp';
 import project9Image from '../assets/employee-tracker.webp';
@@ -17,44 +17,44 @@ const projects = [
     {
         id: 1,
         image: project1Image,
+        title: 'Financial Landing Page',
+        deployedLink: 'https://financial-landing-page.vercel.app/',
+        githubLink: 'https://github.com/RoryDowse/financial-landing-page',
+    },
+    {
+        id: 2,
+        image: project2Image,
         title: 'Dictionary Database',
         deployedLink: 'https://dictionary-db.onrender.com',
         githubLink: 'https://github.com/RoryDowse/Dictionary-DB',
     },
     {
-        id: 2,
-        image: project2Image,
+        id: 3,
+        image: project3Image,
         title: 'DevDemand',
         deployedLink: 'https://devdemand.onrender.com/',
         githubLink: 'https://github.com/RoryDowse/DevDemand',
     },
     {
-        id: 3,
-        image: project3Image,
+        id: 4,
+        image: project4Image,
         title: 'FinanceFlow',
         deployedLink: 'https://finance-flow-f2zu.onrender.com/',
         githubLink: 'https://github.com/RoryDowse/finance-flow',
     },
     {
-        id: 4,
-        image: project4Image,
+        id: 5,
+        image: project5Image,
         title: 'Car Loan Payment Calculator',
         deployedLink: 'https://rorydowse.github.io/car-loan-payment/',
         githubLink: 'https://github.com/RoryDowse/car-loan-payment',
     },
     {
-        id: 5,
-        image: project5Image,
+        id: 6,
+        image: project6Image,
         title: 'James Woodrow Violin',
         deployedLink: 'https://jameswoodrowviolin.com/',
         githubLink: 'https://github.com/RoryDowse/james-woodrow-violin',
-    },
-    {
-        id: 6,
-        image: project6Image,
-        title: 'Weather Dashboard',
-        deployedLink: 'https://weather-dashboard-wuv2.onrender.com',
-        githubLink: 'https://github.com/RoryDowse/weather-dashboard',
     },
     {
         id: 7,
